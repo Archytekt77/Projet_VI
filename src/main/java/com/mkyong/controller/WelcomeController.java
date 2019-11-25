2984b95cdf27af337b5e300720967bbb3350e7d8
Archytekt77 <loic.maria.pro@gmail.com>
2019-11-26 00:05:06 +0100

    Tentative implémentation détails

--- src/main/java/com/mkyong/controller/WelcomeController.java
+++ src/main/java/com/mkyong/controller/WelcomeController.java
@@ -1,30 +1,35 @@
 package com.mkyong.controller;
 
+import com.mkyong.entity.Star;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.templateparser.reader.ParserLevelCommentTextReader;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.time.LocalDate;
+import java.util.*;
 
 @Controller
 public class WelcomeController {
-
-    private List<String> acteurAmericain = new ArrayList<String>() {{
-        add("A");
-        add("B");
-        add("C");
-    }}; /*{{"Lee", "Victor", "John", "Bruce", "Bud", "Christopher", "George", "Norman", "Philip", "Tommy"},
-            {"Aaker", "Aaron", "Abbey", "Abbott", "Abbott", "Abbott", "Abbott", "Abbott", "Abbott", "Abbott"}};*/
+    private List<Star> acteurAmericain = new ArrayList<Star>() {{
+        add(new Star(1, "Aaker", "Lee", LocalDate.of(1943, 9, 25), 76, "Rintintin", false));
+        add(new Star(2, "Aaron", "Victor", LocalDate.of(1943, 9, 25), 58, "Salt", true));
+        add(new Star(3, "Abbey", "John", LocalDate.of(1943, 9, 25), 84, "Mister Freedom", false));
+    }};
 
     @GetMapping("/")
     public String main(Model model) {
         model.addAttribute("acteurAmericain", acteurAmericain);
-
         return "welcome"; //view
     }
 
+    @GetMapping("/details")
+    @ResponseBody
+    public String details(Model model, @RequestParam String id) {
+        model.addAttribute("acteurAmericain", acteurAmericain);
+        return "details";
+    }
 }
--- src/main/java/com/mkyong/entity/Star.java
+++ src/main/java/com/mkyong/entity/Star.java
@@ -1,5 +1,98 @@
 package com.mkyong.entity;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Star {
 
+    private int id;
+    private String nom;
+    private String prenom;
+    private LocalDate dateNaissance;
+    private int age;
+    private String filmCulte;
+    private boolean actif;
+
+    public Star(){
+
+    }
+
+    public Star(int id, String nom, String prenom, LocalDate dateNaissance, int age, String filmCulte, boolean actif) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateNaissance = dateNaissance;
+        this.age = age;
+        this.filmCulte = filmCulte;
+        this.actif = actif;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public LocalDate getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(LocalDate dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFilmCulte() {
+        return filmCulte;
+    }
+
+    public void setFilmCulte(String filmCulte) {
+        this.filmCulte = filmCulte;
+    }
+
+    public boolean isActif() {
+        return actif;
+    }
+
+    public void setActif(boolean actif) {
+        this.actif = actif;
+    }
+
+    @Override
+    public String toString() {
+        return "Star{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", dateNaissance=" + dateNaissance +
+                ", age=" + age +
+                ", filmCulte='" + filmCulte + '\'' +
+                ", actif=" + actif +
+                '}';
+    }
 }
