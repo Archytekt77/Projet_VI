af3d485bc0091f6945bdaea7d86508c5d77c14db
Archytekt77 <loic.maria.pro@gmail.com>
2019-12-23 00:21:52 +0100

    modification controller

--- src/main/java/com/mkyong/controller/StarController.java
+++ src/main/java/com/mkyong/controller/StarController.java
@@ -2,6 +2,7 @@ package com.mkyong.controller;
 
 import com.mkyong.entity.Star;
 import com.mkyong.repository.StarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,55 +15,52 @@ import java.util.*;
 @Controller
 public class StarController {
 
+    @Autowired
     public StarRepository starRepository;
-
-    public StarRepository createStar(){
-        starRepository.save(new Star(1, "Aaker", "Lee", new Date(1943, 9, 25), 76, "Rintintin", false));
-        starRepository.save(new Star(2, "Aaron", "Victor", new Date(1943, 9, 25), 58, "Salt", true));
-        starRepository.save(new Star(3, "Abbey", "John", new Date(1943, 9, 25), 84, "Mister Freedom", false));
-        starRepository.save(new Star(4, "Abbey", "John", new Date(1943, 9, 25), 84, "Mister Freedom", false));
-        return starRepository;
-    }
 
     @GetMapping("/")
     public String main(Model model) {
-        createStar();
-        model.addAttribute("acteursAmericains", );
+        model.addAttribute("acteursAmericains", this.starRepository.findAll());
         return "welcome"; //view
     }
 
     @GetMapping("/details")
-    public String getStarById(Model model, @PathVariable(value = "id") int id) {
-        model.addAttribute("star", starRepository.findById(id));
+    public String getStarById(@PathVariable(value = "id") int id, Model model) {
+        model.addAttribute("star", this.starRepository.findById(id));
         return "details";
     }
 
-
     @GetMapping("/edition")
-    public String edition(Model model, @PathVariable(value = "id") int id) {
-        model.addAttribute("star", starRepository.findById(id));
+    public String edition(@PathVariable(value = "id") int id, Model model) {
+        model.addAttribute("star", this.starRepository.findById(id));
         return "edition";
     }
 
-    @PutMapping("/edition")
-    public String updateStar(@PathVariable(value = "id") int id, Model model){
-        Star star = starRepository.findById(id);
-
-        star.setId(star.getId());
-        star.setNom(star.getNom());
-        star.setPrenom(star.getPrenom());
-        star.setDateNaissance(star.getDateNaissance());
-        star.setAge(star.getAge());
-        star.setFilmCulte(star.getFilmCulte());
-        star.setActif(star.isActif());
-
-        Star updatedStar = starRepository.save(star);
-        return "details";
-    }
-
     @PostMapping("/edition")
-    public String saveEdition(Model model, @ModelAttribute Star star) {
-        System.out.println(star);
+    public String updateStar(@PathVariable(value = "id") int id, Model model, Star star) {
+        starRepository.save(star);
+        model.addAttribute("star", this.starRepository.findById(id));
         return "details";
     }
+
+    /*
+    @GetMapping("/nouvelleStar")
+    public String createStar(){
+        return "nouvelleStar";
+    }
+
+    @PostMapping("/nouvelleStar")
+    public String addStar(@ModelAttribute Star star, Model model, Star star){
+        starRepository.save(star);
+        return "welcome";
+    }
+
+    @PostMapping("")
+    public String deleteStar(@PathVariable(value = "id") int id, Model model, Star star){
+        model.addAttribute("star", starRepository.findById(id));
+
+        starRepository.delete(star);
+        return "welcome";
+    }*/
+
 }
